86fe372270f82e68bec32210d5b576a0e1032215
Hidorion <67104126+Hidorion@users.noreply.github.com>
2021-01-09 12:02:09 +0100

    db

--- Harmony/login/models.py
+++ Harmony/login/models.py
@@ -4,7 +4,21 @@ from django.contrib.auth.models import User
 class UserData(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     department = models.ForeignKey('filters.Department', on_delete=models.CASCADE)
-    asso = models.ManyToManyField('login.Association')
+    asso = models.ManyToManyField('login.Association', blank=True,null=True )
+
+    def __str__(self):
+        """
+        """
+
+        return f"{self.user}"
+
+
+    class Meta:
+        """
+        """
+
+        verbose_name = "Utilisateur"
+        verbose_name_plural = "Utilisateurs"
 
 class Association(models.Model):
     category = models.ManyToManyField('filters.Category')
