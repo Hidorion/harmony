ecd0a6a09d461a6550a38fa47e2b4d5b09f0491f
Hidorion <67104126+Hidorion@users.noreply.github.com>
2021-01-25 10:33:46 +0100

    [IMP] DB model and migrations

--- Harmony/login/models.py
+++ Harmony/login/models.py
@@ -5,7 +5,6 @@ from django.forms import ModelForm
 class UserData(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     department = models.ForeignKey('filters.Department', on_delete=models.CASCADE)
-    asso = models.ManyToManyField('login.Association', blank=True )
 
     def __str__(self):
         """
@@ -17,11 +16,11 @@ class UserData(models.Model):
     class Meta:
         """
         """
-
         verbose_name = "Utilisateur"
         verbose_name_plural = "Utilisateurs"
 
 class Association(models.Model):
+    member = models.ManyToManyField(User)
     logo = models.URLField(max_length=1000, null="img/Harmony.png")
     name = models.CharField(max_length=100)
     category = models.ManyToManyField('filters.Category')
